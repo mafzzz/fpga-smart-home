a88b0cb4c6ac63154720d918bb55ff360ea84f53
UA3MQJ@gmail.com <UA3MQJ@gmail.com@70c7add9-7350-8418-ae17-1c208ab51dd7>
2014-10-28 18:41:22 +0000

    warning fix part2

--- trunk/stepdirdriver.v
+++ trunk/stepdirdriver.v
@@ -2,30 +2,26 @@ module stepdirdriver (input STEP, input DIR,output reg [3:0] OUT);
 reg [3:0] state;
 initial
 begin
-state=0;
-  end
-
-
-  
-  always@(posedge STEP)
-begin
-if (DIR==1) begin
-               state=state+1'b1;
-end
-if (DIR==0) begin
-               state=state-1'b1;
+	state=0;
 end
 
-	  case (state)
-  0: OUT=4'b1000;
-  1: OUT=4'b1100;
-  2: OUT=4'b0100;
-  3: OUT=4'b0110;
-  4: OUT=4'b0010;
-  5: OUT=4'b0011;
-  6: OUT=4'b0001;
-  7: OUT=4'b1001;
-     endcase
-	  	  end
+always@(posedge STEP) begin
+	if (DIR==1) begin
+		state=state+1'b1;
+	end else if (DIR==0) begin
+		state=state-1'b1;
+	end
+
+	case (state)
+	  0: OUT=4'b1000;
+	  1: OUT=4'b1100;
+	  2: OUT=4'b0100;
+	  3: OUT=4'b0110;
+	  4: OUT=4'b0010;
+	  5: OUT=4'b0011;
+	  6: OUT=4'b0001;
+	  7: OUT=4'b1001;
+   endcase
+end
 		  
 endmodule
