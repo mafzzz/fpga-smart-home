57d63e9eb340793cf5a725914817f24750f6c810
UA3MQJ <UA3MQJ@gmail.com>
2014-10-28 17:59:07 +0000

    fix warnings - part1

--- five_sec.v
+++ five_sec.v
@@ -11,14 +11,16 @@ end
 
 always @(posedge clk1M) begin
 
-if(five_sec==5000000)begin
-	five_sec=0;
-	flag_five_sec=1;
-end else if(five_sec==2500000)begin
-flag_five_sec=0;
-five_sec=five_sec+1;
+	if(five_sec==5000000)begin
+		five_sec=1'b0;
+		flag_five_sec=1'b1;
+	end else if(five_sec==2500000)begin
+		flag_five_sec=1'b0;
+		five_sec=five_sec+1'b1;
 	end else begin
-	five_sec=five_sec+1;
+		five_sec=five_sec+1'b1;
 	end
+
 end
-	endmodule
+
+endmodule
--- frqdiv.v
+++ frqdiv.v
@@ -1,4 +1,4 @@
-module  freqdiv (clk50M,clk1hz,clk25M,clk1M);
+module  frqdiv (clk50M,clk1hz,clk25M,clk1M);
 input clk50M;
 output reg clk1hz;
 output reg clk25M;
@@ -25,14 +25,14 @@ if (i == 50000)
 		clk1hz = ~clk1hz;
 		i=0;
 end
-j=j+1;
+j=j+1'b1;
 if (j == 2) 
 	begin
 		clk25M = ~clk25M;
 		j=0;
 	end
 
-z=z+1;
+z=z+1'b1;
 if (z == 25) 
 	begin
 		clk1M = ~clk1M;
