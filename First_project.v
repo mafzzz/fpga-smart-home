b9241a235e4d1c8e9e35f22ede330c74c41e4300
Feodor740 <Feodor740@gmail.com>
2014-10-31 19:58:11 +0000

    mstate work correctly now.

--- First_project.v
+++ First_project.v
@@ -44,7 +44,7 @@ module First_project(input wire key0,
 	  stepdirdriver SDRV(STEP, DIR, STEP_OUT);
 	  Energy_saver  E_s(clk1hz,M_EN,STEP_OUT,OUT_final); 
 	  PWM           p_w_m(clk25M,byte_data_received,PWM_out,PWM_out2,PWM_out3,PWM_out4,PWM_out5,PWM_out6,PWM_out7,PWM_out8,PWM_out9,PWM_out_vent); 
-	  Humidity      Hum(clk1M,Data_H,flag_five_sec,HYM2,mstate,gorb);
+	  Humidity      Hum(clk1M,Data_H,flag_five_sec,HYM2,mstate,Data_H_test);
 	  five_sec      F_S(clk1M,flag_five_sec);
 	  
 	  
