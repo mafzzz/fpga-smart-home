49d381a9bf4a34e877d467d6a637784ff09a5a2f
UA3MQJ <UA3MQJ@gmail.com>
2014-11-09 21:37:00 +0000

    asuncronius fixius

--- five_sec.v
+++ five_sec.v
@@ -3,22 +3,21 @@ input clk1M;
 output reg flag_five_sec;
 reg [22:0] five_sec;
 
-initial
-begin
-five_sec=0;
-flag_five_sec=1;
+initial begin
+	five_sec<=0;
+	flag_five_sec<=1;
 end
 
 always @(posedge clk1M) begin
 
 	if(five_sec==5000000)begin
-		five_sec=1'b0;
-		flag_five_sec=1'b1;
+		five_sec<=1'b0;
+		flag_five_sec<=1'b1;
 	end else if(five_sec==2500000)begin
-		flag_five_sec=1'b0;
 		five_sec=five_sec+1'b1;
+		flag_five_sec<=1'b0;
 	end else begin
-		five_sec=five_sec+1'b1;
+		five_sec<=five_sec+1'b1;
 	end
 
 end
